ab67ed92aa4f1e057a57aef0acbbc961c69c1f9d
Will Turner <turnejam@gmail.com>
2017-12-21 05:49:24 -0500

    Checkpoint 16

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'random_data'
+
+#Create posts
+50.times do
+  Post.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+#Create comments
+
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seeds finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
