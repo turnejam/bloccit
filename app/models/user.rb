a72a29c7bcee51e86334c5de131f7f4896d036ce
Will Turner <turnejam@gmail.com>
2018-01-27 17:33:22 -0500

    Assignment Complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
+  before_save :capitalize_name
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -10,4 +11,12 @@ class User < ApplicationRecord
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  def capitalize_name
+    if name
+      split_name = name.split
+      split_name.map! { |word| word.capitalize}
+      self.name = split_name.join(" ")
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,6 +31,12 @@ RSpec.describe User, type: :model do
       expect(user_with_invalid_name).to_not be_valid
     end
 
+    it "should format the user's name" do
+      user.name = "will turner"
+      user.save
+      expect(user.name).to eq "Will Turner"
+    end
+
     it "should be an invalid user due to blank email" do
       expect(user_with_invalid_email).to_not be_valid
     end
