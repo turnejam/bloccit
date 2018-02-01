6a432a26f3bd100b9403755fec5fbc8cbe473d64
Will Turner <turnejam@gmail.com>
2018-02-01 16:08:26 -0500

    Checkpoint 26 Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -12,19 +22,13 @@ topics = Topic.all
 #Create posts
 50.times do
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
 end
 posts = Post.all
-
-#Unique post
-post = Post.find_or_create_by!(
-  topic: topics.sample,
-  title: "Here's a unique test",
-  body: "Here's the body of that test"
-)
 
 #Create comments
 100.times do
@@ -34,12 +38,15 @@ post = Post.find_or_create_by!(
   )
 end
 
-Comment.find_or_create_by!(
-  post: post,
-  body: "Unique comment"
-)
+#Unique user
+user = User.first
+ user.update_attributes!(
+   email: 'turnejam@gmail.com',
+   password: 'helloworld'
+ )
 
 puts "Seeds finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
