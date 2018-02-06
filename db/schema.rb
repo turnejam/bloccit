cffaca24f3bcbfc4b08daff8a4003ddcdf51d34a
Will Turner <turnejam@gmail.com>
2018-02-06 10:33:46 -0500

    Checkpoint 27 Complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201201930) do
+ActiveRecord::Schema.define(version: 20180205171014) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20180201201930) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,12 +38,20 @@ posts = Post.all
   )
 end
 
-#Unique user
-user = User.first
- user.update_attributes!(
-   email: 'turnejam@gmail.com',
-   password: 'helloworld'
- )
+# Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
 
 puts "Seeds finished"
 puts "#{User.count} users created"
